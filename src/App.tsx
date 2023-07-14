39a5a9297fcaf3c7765a63da90b473b88db96499
Daniel Ji <jidaniel1234@gmail.com>
2023-07-13 21:31:40 -0700

    initial site setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './App.css'
+
+export class App extends Component {
+	render() {
+		return (
+			<div className="app">
+				<h1 className="mt-5 w-100 text-center">TN-93 Web App</h1>
+			</div>
+		)
+	}
+}
+
+export default App
