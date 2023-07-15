8532013bc9a09706eceb0ddf0d4f2f029bfaac5f
Daniel Ji <jidaniel1234@gmail.com>
2023-07-15 13:23:40 -0700

    try to fix github pages deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/tn93-webapp/'
+	base: '/tn93-webapp/',
+	plugins: [react()],
 })
